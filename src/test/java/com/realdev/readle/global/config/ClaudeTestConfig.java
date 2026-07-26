e120b83864c385b2650f902ecfc239ca80553a76
seoilhyeon <ilhyeon0326@gmail.com>
2026-07-26 20:12:44 +0900

    feat: 클로드 API 커스텀 메트릭 도입 (#121)

--- src/test/java/com/realdev/readle/global/config/ClaudeTestConfig.java
+++ src/test/java/com/realdev/readle/global/config/ClaudeTestConfig.java
@@ -1,5 +1,7 @@
 package com.realdev.readle.global.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -8,6 +10,11 @@ import org.springframework.web.client.RestClient;
 
 @TestConfiguration
 public class ClaudeTestConfig {
+
+  @Bean
+  public MeterRegistry meterRegistry() {
+    return new SimpleMeterRegistry();
+  }
 
   @Bean
   @Primary
